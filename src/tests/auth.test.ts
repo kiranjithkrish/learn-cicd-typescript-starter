13b363f644403836e3eb2c8b7abb92477dcc0ade
Code0Light1 <kirankk105@gmail.com>
2025-07-13 15:00:13 +0100

    Fix: typo in tests

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -4,12 +4,12 @@ import { IncomingHttpHeaders } from "http";
 
 
 describe("getAPIKey", () => {
-    it("returns api key when header is present"), () => {
+    it("returns api key when header is present", () => {
         const headers: IncomingHttpHeaders = {
             authorization: "ApiKey my-secret-key"
         };
-        expect(getAPIKey(headers)).toBeNull();
-    }
+        expect(getAPIKey(headers)).toBe("-secret-key")
+    })
 
     it("returns null if authorization header is missing", () => {
         const headers: IncomingHttpHeaders = {};
@@ -24,17 +24,3 @@ describe("getAPIKey", () => {
     })
 })
 
-const person = {
-  isActive: true,
-  age: 32,
-};
-
-describe("person", () => {
-  test("person is defined", () => {
-    expect(person).toBeDefined();
-  });
-
-  test("is active", () => {
-    expect(person.isActive).toBeTruthy();
-  });
-});
