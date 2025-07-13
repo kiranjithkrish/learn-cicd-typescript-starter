4111ce70911190b81297b91a89f9edec82ba8d22
Code0Light1 <kirankk105@gmail.com>
2025-07-13 16:47:39 +0100

    Added the prettier package for foramtting and add a new style job to ci config

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,25 +2,23 @@ import { describe, expect, it, test } from "vitest";
 import { getAPIKey } from "src/api/auth";
 import { IncomingHttpHeaders } from "http";
 
-
 describe("getAPIKey", () => {
-    it("returns api key when header is present", () => {
-        const headers: IncomingHttpHeaders = {
-            authorization: "ApiKey my-secret-key"
-        };
-        expect(getAPIKey(headers)).toBe("my-secret-key")
-    })
+  it("returns api key when header is present", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "ApiKey my-secret-key",
+    };
+    expect(getAPIKey(headers)).toBe("my-secret-key");
+  });
 
-    it("returns null if authorization header is missing", () => {
-        const headers: IncomingHttpHeaders = {};
-        expect(getAPIKey(headers)).toBeNull();
-    })
+  it("returns null if authorization header is missing", () => {
+    const headers: IncomingHttpHeaders = {};
+    expect(getAPIKey(headers)).toBeNull();
+  });
 
-    it("returns null if authorization header is malformed", () => {
-        const headers: IncomingHttpHeaders = {
-            authorization: "Bearer my-sfsdfdsf-key"
-        };
-        expect(getAPIKey(headers)).toBeNull();
-    })
-})
-
+  it("returns null if authorization header is malformed", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "Bearer my-sfsdfdsf-key",
+    };
+    expect(getAPIKey(headers)).toBeNull();
+  });
+});
